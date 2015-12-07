fd22d4dc0a09afa1c3062e30b4e4261fdf0c6ff3
robotechredmond <keith@keithmayer.com>
2015-12-07 07:47:00 -0500

    Uploaded project

---
+++ SecuritySystemUWP/SecuritySystemUWP/MainPage.xaml.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Threading;
+using System.Diagnostics;
+using System.Linq;
+using System.Threading.Tasks;
+using Windows.UI.Xaml;
+using Windows.UI.Xaml.Controls;
+using Windows.UI.Xaml.Navigation;
+
+namespace SecuritySystemUWP
+{
+    /// <summary>
+    /// Main page of the app
+    /// </summary>
+    public sealed partial class MainPage : Page
+    {
+        public MainPage()
+        {
+            string ipAddress = "";
+            string instructions = "";
+
+            this.InitializeComponent();
+
+            // get device environment info and display it in UI
+            deviceNameValueTextBlock.Text = EnvironmentSettings.GetDeviceName();
+            ipAddress = EnvironmentSettings.GetIPAddress();
+            ipAddressValueTextBlock.Text = ipAddress;
+            appVersionValueTextBlock.Text = EnvironmentSettings.GetAppVersion();
+            OSVersionValueTextBlock.Text = EnvironmentSettings.GetOSVersion();
+
+            // instructions text with ip address
+            instructions = "Setup Instructions: To configure this security system please go to URL http://" + ipAddress + ":8000 on a browser.";
+            instructionsTextBlock.Text = instructions;
+
+        }
+    }
+}
---
+++ SecuritySystemUWP/SecuritySystemUWP/Storage/IStorage.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Windows.Storage;
+
+namespace SecuritySystemUWP
+{
+    public interface IStorage
+    {
+        void UploadPictures(string camera);
+        DateTime LastUploadTime { get; }
+        void DeleteExpiredPictures(string camera);
+        void Dispose();
+    }
+}
